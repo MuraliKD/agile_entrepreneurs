6871fc03946c1bc767c1e798feb923d5b83463cf
Prelang Builder <builder@prelang.com>
2015-07-31 00:06:27 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Cofounder' to allow for create, update

--- app/admin/cofounder.rb
+++ app/admin/cofounder.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Cofounder do
+  permit_params :name, :email
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
